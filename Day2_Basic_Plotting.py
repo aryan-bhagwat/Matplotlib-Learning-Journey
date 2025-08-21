29e47ba9b084a965cf4fc5d1de599c80b28a2433
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-08-21 18:29:49 +0530

    Day - 2 Basic Plotting

---
+++ Day2_Basic_Plotting.py
@@ -0,0 +1,30 @@
+# Day 2 – Basic Plotting
+
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y1 = [i**2 for i in x]   # squares
+y2 = [i**3 for i in x]   # cubes
+
+# Plot squares
+plt.plot(x, y1, label="Squares")
+
+# Plot cubes
+plt.plot(x, y2, label="Cubes")
+
+# Labels
+plt.xlabel("X values")
+plt.ylabel("Y values")
+
+# Title
+plt.title("Squares and Cubes")
+
+# Grid
+plt.grid(True)
+
+# Legend
+plt.legend()
+
+# Show plot
+plt.show()
